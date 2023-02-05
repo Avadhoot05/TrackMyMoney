ed93bc685a5596b2943a36e7bb98f0ec52106308
Avadhoot05 <khedekaravadhoot@gmail.com>
2023-02-05 21:38:10 +0530

    fixed _dirname

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ import fileUpload from 'express-fileupload';
 import router from "./routes.js";
 import bodyParser from "body-parser";
 import * as path from 'path';
+
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config({ path: "./config.env" });
 
 const app = express();
