d73a41a68b5f264bc1315bc2eee67589dd417aae
Avadhoot05 <khedekaravadhoot@gmail.com>
2023-02-05 21:09:25 +0530

    update routes in vercel

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ import "./db/conn.js";
 const port = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static("./frontend/build"));
 
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/frontend', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'));
   })
 }
 
